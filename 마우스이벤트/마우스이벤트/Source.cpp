f20d27040b2ecc0802e1f83f650c81350cc8a414
younsoo <younsoo7795@gmail.com>
2022-04-06 01:21:52 +0900

    첫커밋

---
+++ 마우스이벤트/마우스이벤트/Source.cpp
@@ -0,0 +1,76 @@
+#include <windows.h>
+#include <TCHAR.H>
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg,
+	WPARAM wParam, LPARAM lParam);
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpszCmdLine, int nCmdShow)
+{
+	HWND 	 hwnd;
+	MSG 	 msg;
+	WNDCLASS WndClass;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hInstance = hInstance;
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.lpszMenuName = NULL;
+	WndClass.lpszClassName = _T("Window Class Name");
+	RegisterClass(&WndClass);
+	hwnd = CreateWindow(_T("Window Class Name"),
+		_T("Window Title Name"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+		);
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg,
+	WPARAM wParam, LPARAM lParam)
+{
+	HDC			hdc;
+	PAINTSTRUCT ps;
+	static int dmx, dmy, umx, umy;	//마우스 눌렀을때 떼었을때 xy좌표
+
+	switch (iMsg)
+	{
+	case WM_CREATE:
+		break;
+	case WM_LBUTTONDOWN:			//마우스 눌렀을때 이벤트
+		dmx = LOWORD(lParam);
+		dmy = HIWORD(lParam);
+		break;
+	case WM_LBUTTONUP:				//마우스 떼었을때 이벤트
+		umx = LOWORD(lParam);
+		umy = HIWORD(lParam);
+		InvalidateRgn(hwnd, NULL, TRUE);	//pain 이벤트 호출
+		break;
+
+	case WM_PAINT:
+		hdc = BeginPaint(hwnd, &ps);
+		MoveToEx(hdc, dmx, dmy, false);
+		LineTo(hdc, umx, umy);
+		EndPaint(hwnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
